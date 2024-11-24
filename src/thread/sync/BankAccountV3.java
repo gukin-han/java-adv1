6eba01cfb59205ec4cb4f03a406004fe51c8d895
Gukin Han <gukin.dev@gmail.com>
2024-11-24 17:52:17 +0900

    synchronized 코드 블럭 end

--- src/thread/sync/BankAccountV3.java
+++ src/thread/sync/BankAccountV3.java
@@ -1,17 +1,18 @@
 package thread.sync;
 
 import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
 
-public class BankAccountV2 implements BankAccount {
+public class BankAccountV3 implements BankAccount {
 
     private int balance;
 
-    public BankAccountV2(int initialBalance) {
+    public BankAccountV3(int initialBalance) {
         this.balance = initialBalance;
     }
 
     @Override
-    public synchronized boolean withdraw(int amount) {
+    public boolean withdraw(int amount) {
 
         //synchronized 단점: 한 번에 하나의 스레드만 실행 가능 -> 성능 하락
         // 필요한 부분 = 꼭 하나의 스레드만 실행되어야 하는 부분
@@ -19,17 +20,18 @@ public class BankAccountV2 implements BankAccount {
 
         log("거래 시작: " + getClass().getSimpleName());
 
-
-
         // === 임계 영역 시작 ===
-        log("[검증 시작] 출금액: " + amount + ", 잔액: " + balance);
-        if (balance < amount) {
-            log("[검증 실패] 출금액: " + amount + ", 잔액: " + balance);
-            return false;
+        synchronized (this) {
+            log("[검증 시작] 출금액: " + amount + ", 잔액: " + balance);
+            if (balance < amount) {
+                log("[검증 실패] 출금액: " + amount + ", 잔액: " + balance);
+                return false;
+            }
+            log("[검증 완료] 출금액: " + amount + ", 잔액: " + balance);
+            sleep(1000);
+            balance = balance - amount;
+            log("[출금 완료] 출금액: " + amount + ", 변경 잔액: " + balance);
         }
-        log("[검증 완료] 출금액: " + amount + ", 잔액: " + balance);
-        balance = balance - amount;
-        log("[출금 완료] 출금액: " + amount + ", 변경 잔액: " + balance);
         // === 임계 영역 종료 ===
 
         log("거래 종료");
